714d499603b58648a64f313a9a7b267230ab99e5
Edmilson Frank <edmilson0@gmail.com>
2017-06-07 22:20:02 -0300

    p10 solved

--- src/problems/Problem10.java
+++ src/problems/Problem10.java
@@ -10,7 +10,31 @@ public class Problem10 {
 	 * Find the sum of all the primes below two million.
 	 */
 	public static void main(String[] args) {
-		BigInteger limit = new BigInteger("2000000");
+		int limit = 2000000;
+		
+		boolean[] numbers = new boolean[limit + 1];
+		for (int i = 0; i <= limit; i++) {
+			numbers[i] = true;
+		}
+		
+		BigInteger sum = BigInteger.ZERO;
+		for (int i = 2; i <= limit; i++) {
+			int pos = i*2;
+			if (numbers[i]) sum = sum.add(BigInteger.valueOf(i));
+			while (pos <= limit) {
+				numbers[pos] = false;
+				pos += i;
+			}
+		}
+		
+		showArray(numbers);
+		System.out.println("Sum: " + sum);
+	}
+
+	private static void showArray(boolean[] numbers) {
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i]) System.out.printf("Number %d: %b\n", i, numbers[i]);
+		}
 	}
 	
 }
